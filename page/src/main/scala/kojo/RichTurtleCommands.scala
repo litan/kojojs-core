243f76eb363ef2d5fd5bcf5a97b564ef75efab16
Lalit Pant <pant.lalit@gmail.com>
2018-01-18 22:44:17 +0530

    Add setSpeed() command.

--- page/src/main/scala/kojo/RichTurtleCommands.scala
+++ page/src/main/scala/kojo/RichTurtleCommands.scala
@@ -3,6 +3,7 @@ package kojo
 trait RichTurtleCommands {
   def turn(angle: Double): Unit
   def forward(n: Double): Unit
+  def setAnimationDelay(i: Long)
 
   def left(angle: Double): Unit = turn(angle)
   def right(angle: Double)      = turn(-angle)
@@ -13,6 +14,18 @@ trait RichTurtleCommands {
   def forward(): Unit = forward(25)
   def back(): Unit    = back(25)
 
+  trait Speed
+  case object slow      extends Speed
+  case object medium    extends Speed
+  case object fast      extends Speed
+  case object superFast extends Speed
+
+  def setSpeed(speed: Speed) = speed match {
+    case `slow`      => setAnimationDelay(1000)
+    case `medium`    => setAnimationDelay(100)
+    case `fast`      => setAnimationDelay(10)
+    case `superFast` => setAnimationDelay(0)
+  }
 //  def arc2(r: Double, a: Double): Unit = throw new UnsupportedOperationException("making arcs is not supported")
 //  def left(angle: Double, rad: Double): Unit = arc2(rad, angle)
 //  def right(angle: Double, rad: Double): Unit = { if (angle == 0) return; right(180); arc2(rad, -angle) }
