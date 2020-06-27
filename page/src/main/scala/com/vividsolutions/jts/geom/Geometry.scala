f38ff19eac80bfb395971ff63835cfa2fcef710d
Lalit Pant <pant.lalit@gmail.com>
2020-06-27 13:40:38 +0530

    Sync with kojojs-dev repo.

--- page/src/main/scala/com/vividsolutions/jts/geom/Geometry.scala
+++ page/src/main/scala/com/vividsolutions/jts/geom/Geometry.scala
@@ -6,8 +6,8 @@ import scala.scalajs.js.annotation.JSGlobal
 @js.native
 @JSGlobal("jsts.geom.Geometry")
 class Geometry extends js.Object {
-  def intersects(other: Geometry): Boolean    = js.native
-  def union(other: Geometry): Geometry        = js.native
-  def getCoordinates(): js.Array[Coordinate]  = js.native
+  def intersects(other: Geometry): Boolean = js.native
+  def union(other: Geometry): Geometry = js.native
+  def getCoordinates(): js.Array[Coordinate] = js.native
   def intersection(other: Geometry): Geometry = js.native
 }
